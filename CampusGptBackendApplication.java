266e9b05313c98401b29ead81af89748b51ff36e
Dhinakaran <dhinakaransivam2004@gmail.com>
2026-07-16 14:07:43 +0530

    Add files via upload

---
+++ CampusGptBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGptBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusGptBackendApplication.class, args);
+	}
+
+}
